0cc96a4a2bad520cd0c1ba397f1902a162d0b485
charleskahuho <ckcharles004@gmail.com>
2023-07-04 09:52:21 +0300

    comments added

--- .vscode/assignment_group/op_overloading.py
+++ .vscode/assignment_group/op_overloading.py
@@ -1,9 +1,11 @@
 class Point: # creating a class called point
-    def __init__(self, x, y): # line initiates the class defination fot the point class
-        #cunstructor(__init method__)
+    def __init__(self, x, y): # line initiates the class defination fot the point class self represents the instance for the class itself. x &y used to intialize the attributes of the object
+        #cunstructor(__init method__) 
         self.x = x
         self.y = y
-
+# this method is used to overload + operator for the class
+#self represents the first point 'point1' and other represents the other point 'point2'
+#this method creates anew point by adding x&y cordinates of the two points
     def __add__(self, other):
         return Point(self.x + other.x, self.y + other.y)
     
